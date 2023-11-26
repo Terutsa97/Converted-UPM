4b9f35ace1a0bb26e0d8b050c0e887d382b7b53c
Terutsa97 <51282156+Terutsa97@users.noreply.github.com>
2023-11-26 00:50:58 -0500

    Moved to Namespace, added validators and implemented merge folders function

--- Editor/FolderUtilities.cs
+++ Editor/FolderUtilities.cs
@@ -8,135 +8,217 @@ using UnityEngine;
 
 using UnityHierarchyFolders.Runtime;
 
-public class FolderUtilities : ScriptableObject
+namespace Terutsa97.FolderUtilities
 {
-    [MenuItem("GameObject/Folder Utilities/Convert Empty To Folder", false, 1)]
-    public static void ConvertEmptyToFolder(MenuCommand command)
+    public static class FolderUtilities
     {
-        var selectedParent = ((GameObject)command.context).transform;
-        var childTransforms = ((GameObject)command.context).GetComponentsInChildren<Transform>();
+        const string PARENT_FOLDER = "GameObject/Folder Utilities/";
+        static int s_processedItemCount = 0;
 
-        if (selectedParent == null) { return; }
-
-        if (selectedParent.childCount == 0) { return; }
-
-        Undo.IncrementCurrentGroup();
-
-        var newFolder = new GameObject(selectedParent.name);
-        Undo.RegisterCreatedObjectUndo(newFolder, "Created New Folder");
-
-        var index = Selection.activeTransform.GetSiblingIndex();
-
-        if (selectedParent.parent != null)
+        #region Convert Empty To Folder
+        const string CONVERT_EMPTY_NAME = "Convert Empty To Folder";
+        [MenuItem(PARENT_FOLDER + CONVERT_EMPTY_NAME, false, 1)]
+        public static void ConvertEmptyToFolder(MenuCommand command)
         {
-            Undo.SetTransformParent(newFolder.transform, selectedParent.parent, "Modify parent");
-            ClearTransform(newFolder.transform);
+            var selectedParent = ((GameObject)command.context).transform;
+            var childTransforms = ((GameObject)command.context).GetComponentsInChildren<Transform>();
+
+            Undo.IncrementCurrentGroup();
+
+            var newFolder = new GameObject(selectedParent.name);
+            Undo.RegisterCreatedObjectUndo(newFolder, "Created New Folder");
+
+            var index = Selection.activeTransform.GetSiblingIndex();
+
+            if (selectedParent.parent != null)
+            {
+                Undo.SetTransformParent(newFolder.transform, selectedParent.parent, "Modify parent");
+                ClearTransform(newFolder.transform);
+            }
+
+            newFolder.transform.SetSiblingIndex(index);
+
+            int i = 0;
+            foreach (var child in childTransforms)
+            {
+                Undo.SetTransformParent(child, newFolder.transform, $"Modify parent of: {child.name} {i}");
+                i++;
+            }
+
+            Undo.DestroyObjectImmediate(selectedParent.gameObject);
+            Selection.activeTransform = newFolder.transform;
+
+            Undo.AddComponent<Folder>(newFolder);
+
+            Undo.SetCurrentGroupName("Reset Parent Transform");
         }
 
-        newFolder.transform.SetSiblingIndex(index);
+        [MenuItem(PARENT_FOLDER + CONVERT_EMPTY_NAME, true)]
+        static bool Validate_ConvertEmptyToFolder()
+            => Selection.activeTransform != null
+            && Selection.activeTransform.childCount != 0
+            && Selection.activeTransform.GetComponent<Folder>() == null;
+        #endregion
 
-        int i = 0;
-        foreach (var child in childTransforms)
+        #region Convert Folder To Empty
+        const string CONVERT_FOLDER_NAME = "Convert Folder To Empty";
+        [MenuItem(PARENT_FOLDER + CONVERT_FOLDER_NAME, false, 1)]
+        public static void ConvertFolderToEmpty(MenuCommand command)
         {
-            Undo.SetTransformParent(child, newFolder.transform, $"Modify parent of: {child.name} {i}");
-            i++;
+            var selectedParent = ((GameObject)command.context).transform;
+            var childTransforms = ((GameObject)command.context).GetComponentsInChildren<Transform>();
+
+            Undo.IncrementCurrentGroup();
+
+            var newEmpty = new GameObject(selectedParent.name);
+            Undo.RegisterCreatedObjectUndo(newEmpty, "Created New Empty");
+
+            var index = Selection.activeTransform.GetSiblingIndex();
+
+            if (selectedParent.parent != null)
+            {
+                Undo.SetTransformParent(newEmpty.transform, selectedParent.parent, "Modify parent");
+                ClearTransform(newEmpty.transform);
+            }
+
+            newEmpty.transform.SetSiblingIndex(index);
+
+            int i = 0;
+            foreach (var child in childTransforms)
+            {
+                Undo.SetTransformParent(child, newEmpty.transform, $"Modify parent of: {child.name} {i}");
+                i++;
+            }
+
+            Undo.DestroyObjectImmediate(selectedParent.gameObject);
+            Selection.activeTransform = newEmpty.transform;
+
+            Undo.SetCurrentGroupName("Reset Parent Transform");
         }
 
-        Undo.DestroyObjectImmediate(selectedParent.gameObject);
-        Selection.activeTransform = newFolder.transform;
+        [MenuItem(PARENT_FOLDER + CONVERT_FOLDER_NAME, true)]
+        static bool Validate_ConvertFolderToEmpty()
+            => Selection.activeTransform != null
+            && Selection.activeTransform.GetComponent<Folder>() != null;
+        #endregion
 
-        Undo.AddComponent<Folder>(newFolder);
+        #region Create Folder From Selection
+        const string FOLDER_FROM_SELECTION = "Create A Folder From Selection";
 
-        Undo.SetCurrentGroupName("Reset Parent Transform");
-    }
-
-    private static int _processedItemCount = 0;
-
-    [MenuItem("GameObject/Folder Utilities/Create Folder From Selection", false, 1)]
-    public static void CreateFolderFromSelection()
-    {
-        var selectedObjects = Selection.transforms;
-
-        if (selectedObjects.Length == 0) { return; }
-
-        if (_processedItemCount < selectedObjects.Length - 1)
+        [MenuItem(PARENT_FOLDER + FOLDER_FROM_SELECTION, false, 1)]
+        public static void CreateFolderFromSelection()
         {
-            _processedItemCount++;
-            return;
+            var selectedObjects = Selection.transforms;
+
+            if (s_processedItemCount < selectedObjects.Length - 1)
+            {
+                s_processedItemCount++;
+                return;
+            }
+
+            s_processedItemCount = 0;
+
+            Undo.IncrementCurrentGroup();
+
+            var newFolder = new GameObject("Folder");
+            newFolder.AddComponent<Folder>();
+
+            var commonParent = Selection.activeTransform.parent;
+            var index = Selection.activeTransform.GetSiblingIndex();
+
+            GameObjectUtility.SetParentAndAlign(newFolder, commonParent != null ? commonParent.gameObject : null);
+            Undo.RegisterCreatedObjectUndo(newFolder, "Created New Folder");
+
+            newFolder.transform.SetSiblingIndex(index);
+
+            foreach (Transform child in selectedObjects)
+            {
+                Undo.SetTransformParent(child, newFolder.transform, "Modify parent of: " + child.name);
+            }
+
+            Undo.SetCurrentGroupName("Reset Parent Transform");
+            Selection.activeObject = newFolder;
         }
 
-        _processedItemCount = 0;
+        [MenuItem(PARENT_FOLDER + FOLDER_FROM_SELECTION, true)]
+        static bool Validate_CreateFolderFromSelection()
+            => Selection.transforms.Length != 0;
+        #endregion
 
-        Undo.IncrementCurrentGroup();
+        #region Merge Folders Together
+        const string MERGE_FOLDERS_NAME = "Merge Folders";
 
-        var newFolder = new GameObject("Folder");
-        newFolder.AddComponent<Folder>();
-
-        var commonParent = Selection.activeTransform.parent.gameObject;
-        var index = Selection.activeTransform.GetSiblingIndex();
-
-        GameObjectUtility.SetParentAndAlign(newFolder, commonParent);
-        Undo.RegisterCreatedObjectUndo(newFolder, "Created New Folder");
-
-        newFolder.transform.SetSiblingIndex(index);
-
-        foreach (Transform child in selectedObjects)
+        [MenuItem(PARENT_FOLDER + MERGE_FOLDERS_NAME, false, 1)]
+        public static void MergeFoldersTogether()
         {
-            Undo.SetTransformParent(child, newFolder.transform, "Modify parent of: " + child.name);
+            var selectedObjects = Selection.transforms;
+
+            if (s_processedItemCount < selectedObjects.Length - 1)
+            {
+                s_processedItemCount++;
+                return;
+            }
+
+            s_processedItemCount = 0;
+
+            Undo.IncrementCurrentGroup();
+
+            var newFolder = new GameObject("Folder");
+            newFolder.AddComponent<Folder>();
+
+            var commonParent = Selection.activeTransform.parent;
+            var index = Selection.activeTransform.GetSiblingIndex();
+
+            GameObjectUtility.SetParentAndAlign(newFolder, commonParent != null ? commonParent.gameObject : null);
+            Undo.RegisterCreatedObjectUndo(newFolder, "Created New Folder");
+
+            newFolder.transform.SetSiblingIndex(index);
+
+            // Note: Reversed since removing from a list works wonky when starting from 0
+            for (int i = selectedObjects.Length - 1; i >= 0; i--)
+            {
+                for (int j = selectedObjects[i].childCount - 1; j >= 0; j--)
+                {
+                    Transform child = selectedObjects[i].GetChild(j);
+                    child.SetAsFirstSibling();
+                    Undo.SetTransformParent(child, newFolder.transform, "Modify parent of: " + child.name);
+                }
+
+                Undo.DestroyObjectImmediate(selectedObjects[i].gameObject);
+            }
+
+            Undo.SetCurrentGroupName("Reset Parent Transform");
+            Selection.activeObject = newFolder;
         }
 
-        Undo.SetCurrentGroupName("Reset Parent Transform");
-        Selection.activeObject = newFolder;
-    }
+        [MenuItem(PARENT_FOLDER + MERGE_FOLDERS_NAME, true)]
+        static bool Validate_MergeFoldersTogether()
+            => Selection.transforms.Length != 0 
+            & Selection.transforms.All(s => s.GetComponent<Folder>() != null);
+        #endregion
 
-    //[MenuItem("GameObject/Folder Utilities/Merge Folders", false, 111)]
-    public static void MergeFoldersTogether()
-    {
-        // TODO: Implement Logic
+        #region Sort Folders A-Z
+        // TODO: Make Ability to Sort Folders...
+        //const string SORT_FOLDERS_NAME = "Sort Folders [A-Z]";
 
-        //var selectedObjects = Selection.transforms;
-
-        //if (selectedObjects.Length == 0) { return; }
-
-        //if (_processedItemCount < selectedObjects.Length - 1)
+        //[MenuItem(PARENT_FOLDER + SORT_FOLDERS_NAME, false, 1)]
+        //public static void SortFolder()
         //{
-        //    _processedItemCount++;
-        //    return;
         //}
+        #endregion
 
-        //_processedItemCount = 0;
+        #region Helper Methods
+        static void ClearTransform(Transform transform)
+        {
+            Undo.RegisterFullObjectHierarchyUndo(transform, transform.name + "Reset Parent Scale");
+            transform.localScale = Vector3.one;
 
-        //Undo.IncrementCurrentGroup();
+            Undo.RegisterFullObjectHierarchyUndo(transform, transform.name + "Reset Parent Position");
+            transform.localPosition = Vector3.zero;
 
-        //var newFolder = new GameObject("Folder");
-        //newFolder.AddComponent<Folder>();
-
-        //var commonParent = Selection.activeTransform.parent.gameObject;
-        //var index = Selection.activeTransform.GetSiblingIndex();
-
-        //GameObjectUtility.SetParentAndAlign(newFolder, commonParent);
-        //Undo.RegisterCreatedObjectUndo(newFolder, "Created New Folder");
-
-        //newFolder.transform.SetSiblingIndex(index);
-
-        //foreach (Transform child in selectedObjects)
-        //{
-        //    Undo.SetTransformParent(child, newFolder.transform, "Modify parent of: " + child.name);
-        //}
-
-        //Undo.SetCurrentGroupName("Reset Parent Transform");
-        //Selection.activeObject = newFolder;
-    }
-
-    public static void ClearTransform(Transform transform)
-    {
-        Undo.RegisterFullObjectHierarchyUndo(transform, transform.name + "Reset Parent Scale");
-        transform.localScale = Vector3.one;
-
-        Undo.RegisterFullObjectHierarchyUndo(transform, transform.name + "Reset Parent Position");
-        transform.localPosition = Vector3.zero;
-
-        Undo.RegisterFullObjectHierarchyUndo(transform, transform.name + "Reset Parent Rotation");
-        transform.localRotation = Quaternion.identity;
+            Undo.RegisterFullObjectHierarchyUndo(transform, transform.name + "Reset Parent Rotation");
+            transform.localRotation = Quaternion.identity;
+        }
+        #endregion
     }
 }
